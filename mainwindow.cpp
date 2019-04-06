2ded5ccf9f6bf793bbfac5bec5102a4226764be4
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2019-04-06 13:48:56 +0200

    init

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
+#include <iostream>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-        QMainWindow(parent),
-        ui(new Ui::MainWindow)
+        QMainWindow(parent),ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    ui->progressmanual->setMaximum(100);
+    ui->progressmanual->setMinimum(0);
+    ui->progressmanual->setValue(10);
+    connect (ui->btndownloadmanul, SIGNAL( clicked() ), this, SLOT( btnclick() ));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::btnclick() {
+        std::cout << "buttton clicked!\n";
+        ui->progressmanual->setValue(ui->progressmanual->value()+1);
 }

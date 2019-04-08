40cb06ea6891fb2b624306bca0e7999b4f0c7cd2
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2019-04-08 18:55:30 +0200

    implemented multithreading

--- download/DownloadManager.cpp
+++ download/DownloadManager.cpp
@@ -9,69 +9,78 @@
 #include <functional>
 #include "DownloadManager.h"
 
+#include <thread>
+
 int DownloadManager::loadedsize = 0;
 std::vector<std::function<void()>> DownloadManager::listeners;
 
 size_t DownloadManager::write_data(void *buffer, size_t size, size_t buffersize, FILE *stream) {
     size_t written = fwrite(buffer, size, buffersize, stream);
     //loadedsize+= sizeof(buffer);
-    loadedsize+=buffersize;
+    loadedsize += buffersize;
     std::cout << loadedsize << "\n";
     fireEvent();
     return written;
 }
 
+
 void DownloadManager::downloadUrl(std::string url, std::string filename) {
-    CURL *curl;
-    FILE *fp;
-    CURLcode res;
-    curl = curl_easy_init();
-    std::cout << "inited\n";
-    std::cout << url << "<<<< link\n";
-
-    //TODO read somehow the filesize of the music file to download
-    curl_off_t cl;
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-
-    //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-    curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
-
-    curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &cl);
-    res = curl_easy_perform(curl);
-
-    std::cout << "filesize is: " << cl << "\n";
-
-    curl = curl_easy_init();
+    std::thread thread = std::thread([url,filename] {
 
 
-    if (curl) {
-        fp = fopen(filename.c_str(),"wb");
-        //ssl verification
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,1L);
-        curl_easy_setopt(curl,CURLOPT_BUFFERSIZE,1024);
+        CURL *curl;
+        FILE *fp;
+        CURLcode res;
+        curl = curl_easy_init();
+        std::cout << "inited\n";
+        std::cout << url << "<<<< link\n";
 
-        res = curl_easy_perform(curl);
+        //TODO read somehow the filesize of the music file to download
+//        curl_off_t cl;
+//        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+//        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+//        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+//
+//        //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+//        curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+//
+//        curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &cl);
+//        res = curl_easy_perform(curl);
+//
+//        std::cout << "filesize is: " << cl << "\n";
+//
+//        curl = curl_easy_init();
 
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-        fclose(fp);
-        std::cout << "closed\n";
-    }
+
+        if (curl) {
+            fp = fopen(filename.c_str(), "wb");
+            //ssl verification
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, 1024);
+
+            res = curl_easy_perform(curl);
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+            fclose(fp);
+            std::cout << "closed\n";
+        }
+    });
+    thread.detach();
 }
-void DownloadManager::addActionListener(std::function<void()> test){
+
+void DownloadManager::addActionListener(std::function<void()> test) {
     listeners.push_back(test);
 }
 
 void DownloadManager::fireEvent() {
     for (int i = 0; i < listeners.size(); ++i) {
-    listeners.at(i)();
+        listeners.at(i)();
     }
 }
 
